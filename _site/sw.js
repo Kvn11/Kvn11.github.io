2896cbb95cbfd4fd699ccd41c5b83e68b70fded6
Kvn11 <kevin.2.romero@uconn.edu>
2021-08-14 23:21:27 -0400

    testing

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210814.2312';
+const cacheName = 'chirpy-20210814.2321';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
